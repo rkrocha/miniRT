017ca46bf62ec1981727c1d8722b2628b1c4c30b
rkochhan <rkochhan@student.42sp.org.br>
2021-04-14 21:51:39 -0300

    added new functions

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 08:09:28 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/04/14 15:02:05 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/04/14 21:22:40 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,14 +49,19 @@ void	parse_by_type(char *line, t_scene *scene, bool *scene_error);
 */
 bool	get_next_float(float *number, char **line);
 bool	get_next_int(int *number, char **line);
+
+void	parse_invalid_element(char *line, bool *error, int line_num);
 bool	parse_light_ratio(float *ratio, char **line, int line_num);
 bool	parse_rgb(int *color, char **line, int line_num);
+bool	parse_position(t_coord *position, char **line, int line_num);
+bool	parse_orientation(t_coord *orient, char **line, int line_num);
 
 /*
 ** minirt_debug_elements.c:
 */
-void	minirt_debug_line(char *line, int line_num);
-void	minirt_debug_res(char *line, t_scene scene, int line_num);
-void	minirt_debug_ambl(char *line, t_light ambient, int line_num);
+void	debug_line(char *line, int line_num);
+void	debug_res(char *line, t_scene scene, int line_num);
+void	debug_ambl(char *line, t_light ambient, int line_num);
+void	debug_camera(char *line, t_camera camera, int line_num);
 
 #endif

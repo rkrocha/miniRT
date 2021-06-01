ada70c1d9b477690e270883d09b095e1b9e5c710
rkochhan <rkochhan@student.42sp.org.br>
2021-06-01 11:15:31 -0300

    reorganized struct variables

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 08:09:28 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/04/20 16:35:26 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/06/01 09:59:41 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,32 @@
 # include "minirt_types.h"
 # include "mlx.h"
 
-typedef struct	s_image
-{
-	void	*ptr;
-	char	*addr;
-}	t_image;
-
-typedef struct	s_minilibx
+typedef struct s_minilibx
 {
 	void	*ptr;
 	void	*window;
-	t_list	*image;
-	t_image	*display;
+	t_list	*first_cam;
+	t_list	*active_cam;
+	void	*img_ptr;
+	char	*img_addr;
 	int		bits_per_pixel;
 	int		line_len;
 	int		endian;
 }	t_minilibx;
 
 /*
+** minirt_window.c:
+*/
+void	check_adjust_window_resolution(t_minilibx *mlx, t_scene *scene);
+void	display_manager(t_minilibx *mlx);
+
+/*
 ** minirt_mlx_utils.c:
 */
 int		color_picker(t_uchar red, t_uchar green, t_uchar blue);
-void	paint_pixel(t_image *img, int x, int y, int color);
-void	fill_image(t_image *img, int color);
-void	create_image(t_image *img, void *mlx, int width, int height);
+void	create_image(t_minilibx *mlx, int width, int height);
+void	putpixel_image(t_minilibx *mlx, int x, int y, int color);
+void	fill_image(t_minilibx *mlx, t_scene *scene, int color);
 
 /*
 ** minirt_scene.c:
@@ -78,9 +80,9 @@ void	parse_triangle(char *line, t_list **object, bool *error, int line_num);
 */
 void	parse_invalid_element(char *line, bool *error, int line_num);
 void	parse_light_ratio(float *ratio, char *str, bool *error, int line_num);
-void	parse_rgb(int *color, char *str, bool *error, int line_num);
 void	parse_position(t_coord *position, char *str, bool *error, int line_num);
 void	parse_orient(t_coord *orient, char *str, bool *error, int line_num);
+void	parse_rgb(int *color, char *str, bool *error, int line_num);
 
 /*
 ** minirt_debug_elements.c:

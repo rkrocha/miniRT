e7234251ed3a9b5231b3dfad2d6444c2f6e9b752
rkochhan <rkochhan@student.42sp.org.br>
2021-06-02 15:15:58 -0300

    finished hooks

--- minirt.c
+++ minirt.c
@@ -6,30 +6,36 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 08:08:56 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/06/01 10:23:52 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/06/02 14:52:54 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-// static void	define_mlx_hooks(t_minilibx *mlx, t_scene *scene)
-// {
-// 	mlx_hook(mlx->ptr, 33, 1L << 17, close_window, mlx);
-// 	mlx_key_hook(window.ptr, keyboard_input, &window);
-// }
 
 static void	run_mlx(t_scene *scene)
 {
 	t_minilibx	mlx;
 
 	ft_bzero(&mlx, sizeof(mlx));
-	mlx.ptr = mlx_init();
-	check_adjust_window_resolution(&mlx, scene);
-	mlx.first_cam = scene->camera;
+	mlx.ptr = mlx_init(); // 	quit in case of NULL return
+	mlx.scene = scene;
 	mlx.active_cam = scene->camera;
-	create_image(&mlx, scene->render_width, scene->render_height);
+	check_adjust_window_resolution(&mlx, scene);
 	mlx.window = mlx_new_window(mlx.ptr,
 			scene->render_width, scene->render_height, "miniRT");
+
+	if (mlx.active_cam != NULL)
+	{
+		create_image(&mlx);
+		fill_image(&mlx, 0xAAAAAA);
+		((t_camera *)mlx.active_cam->content)->image.is_rendered = true;
+	}
+
+	mlx_key_hook(mlx.window, keyboard_input, &mlx);
+	mlx_expose_hook(mlx.window, refresh_display, &mlx);
+	mlx_hook(mlx.window, 33, 1L << 17, close_window, &mlx);
+
 
 	mlx_loop(mlx.ptr);
 }
@@ -45,7 +51,7 @@ int	main(int argc, const char **argv)
 		ft_bzero(&scene, sizeof(t_scene));
 		parse_scene(argv[1], &scene);
 		run_mlx(&scene);
-		free_scene(&scene);		// REMOVE
+		//free_scene(&scene);		// REMOVE
 	}
 	// else if (argc == 3 && !ft_strncmp(argv[2], "--save", 7))
 	// {
@@ -57,3 +63,5 @@ int	main(int argc, const char **argv)
 		print_minirt_usage();
 	return (0);
 }
+
+// 	reorganize main to avoid duplicate function calls

99cc94a2850f467554f970342cc6ad66a84ebe7a
rkochhan <rkochhan@student.42sp.org.br>
2021-06-19 11:47:18 -0300

    fixed square size in odd angles

--- minirt_raytracing_collision.c
+++ minirt_raytracing_collision.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 18:13:06 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/06/19 11:06:18 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/06/19 11:13:38 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,10 +73,10 @@ bool	rt_square(void *object, t_ray *ray)
 	denom = v_dot(sq->orient, ray->orient);
 	time = v_dot(v_subtract(sq->position, ray->origin), sq->orient) / denom;
 	if ((denom > -FLOAT_EPSILON && denom < FLOAT_EPSILON) ||
-			time > ray->hit_time || time <= 0 ||
+			time > ray->hit_time || time < 0 ||
 			point_in_square(ray, sq, time) == false)
 		return (false);
-	if (ray->hit_time > time && time > 0)
+	else
 	{
 		ray->hit_time = time;
 		ray->hit_point = calc_hit_point(ray);
@@ -84,7 +84,6 @@ bool	rt_square(void *object, t_ray *ray)
 		if (v_dot(ray->orient, sq->orient) > 0)
 			ray->hit_normal = v_scale(ray->hit_normal, -1);
 		ray->hit_color = sq->color;
-		return (true);
 	}
-	return (false);
+	return (true);
 }
--- minirt_raytracing_utils.c
+++ minirt_raytracing_utils.c
@@ -6,7 +6,7 @@
 /*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:36:56 by rkochhan          #+#    #+#             */
-/*   Updated: 2021/06/19 11:09:33 by rkochhan         ###   ########.fr       */
+/*   Updated: 2021/06/19 11:45:13 by rkochhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,12 +43,12 @@ bool	point_in_square(t_ray *ray, t_square *sq, float time)
 
 	hit = v_add(ray->origin, v_scale(ray->orient, time));
 	hit = v_subtract(hit, sq->position);
-	if (sq->orient.y == 1 || sq->orient.y == -1)
+	n = sq->orient;
+	if (n.y == 1 || n.y == -1)
 		u = v_create(1, 0, 0);
 	else
-		u = v_cross(v_create(0, -1, 0), sq->orient);
-	v = v_cross(sq->orient, u);
-	n = sq->orient;
+		u = v_normalize(v_cross(v_create(0, -1, 0), n));
+	v = v_cross(n, u);
 	result.x = u.x * hit.x + v.x * hit.y + n.x * hit.z;
 	result.y = u.y * hit.x + v.y * hit.y + n.y * hit.z;
 	if (fabs(result.x) < (sq->side / 2) && fabs(result.y) < (sq->side / 2))

5e64a8aa45a7d8d73efc56df553b783a5753470a
rkrocha <rafaelkr7@gmail.com>
2022-03-13 09:19:36 -0300

    fixed bool dependency issues

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,1 +1,24 @@
-/home/rafa/Documents/42cursus/get_next_line/get_next_line.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkochhan <rkochhan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 19:22:07 by rkochhan          #+#    #+#             */
+/*   Updated: 2021/04/07 08:49:26 by rkochhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 31
+# endif
+
+# include "libft.h"
+
+int	get_next_line(int fd, char **line);
+
+#endif
